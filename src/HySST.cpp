f7d1261399da951905d8c6f88307d18f9d073ad0
Italo Silva <italo@maleldil.com>
2016-09-15 14:28:49 -0300

    Transforma membros do HySST em const, inicializa vector de heuristicas mutacionais

--- src/HySST.cpp
+++ src/HySST.cpp
@@ -80,19 +80,9 @@ long long HySST::tempo_total() const
     return tempo_total_;
 }
 
-void HySST::set_tempo_total(long long tempo_total)
-{
-    tempo_total_ = tempo_total;
-}
-
 long long HySST::tempo_mutation() const
 {
     return tempo_mutation_;
-}
-
-void HySST::set_tempo_mutation(long long tempo_mutation)
-{
-    tempo_mutation_ = tempo_mutation;
 }
 
 long long HySST::tempo_hill() const
@@ -100,19 +90,9 @@ long long HySST::tempo_hill() const
     return tempo_hill_;
 }
 
-void HySST::set_tempo_hill(long long tempo_hill)
-{
-    tempo_hill_ = tempo_hill;
-}
-
 int HySST::max_level() const
 {
     return max_level_;
-}
-
-void HySST::set_max_level(int max_level)
-{
-    max_level_ = max_level;
 }
 
 std::vector<int> HySST::gen_thresholds(int max_level, int t_start, int t_step)
--- src/HySST.h
+++ src/HySST.h
@@ -12,30 +12,33 @@ public:
     std::unique_ptr<Solucao> gerar_horario(const Solucao& s_inicial) const;
 
     const Resolucao& res() const;
-
     long long tempo_total() const;
-    void set_tempo_total(long long tempo_total);
-
     long long tempo_mutation() const;
-    void set_tempo_mutation(long long tempo_mutation);
-
     long long tempo_hill() const;
-    void set_tempo_hill(long long tempo_hill);
-
     int max_level() const;
-    void set_max_level(int max_level);
 
 private:
     static std::vector<int> gen_thresholds(int max_level, int t_start, int t_step);
     std::unique_ptr<Solucao> aplicar_heuristica(Resolucao::Vizinhanca llh, 
                                                 const Solucao& solucao) const;
 
-    const Resolucao& res_;
-    long long tempo_total_;
-    long long tempo_mutation_;
-    long long tempo_hill_;
-    int max_level_;
-    std::vector<int> thresholds_;
-    std::vector<Resolucao::Vizinhanca> heuristicas_mutacionais_;
-    std::vector<Resolucao::Vizinhanca> heuristicas_hill_;
+    const Resolucao&       res_;
+    const long long        tempo_total_;
+    const long long        tempo_mutation_;
+    const long long        tempo_hill_;
+    const int              max_level_;
+    const std::vector<int> thresholds_;
+
+    const std::vector<Resolucao::Vizinhanca> heuristicas_mutacionais_{
+        Resolucao::Vizinhanca::ES,
+        Resolucao::Vizinhanca::EM,
+        Resolucao::Vizinhanca::RS,
+        Resolucao::Vizinhanca::RM,
+        Resolucao::Vizinhanca::PR,
+        Resolucao::Vizinhanca::RM
+    };
+
+    const std::vector<Resolucao::Vizinhanca> heuristicas_hill_{
+
+    };
 };
