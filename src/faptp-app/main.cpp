6ed7542d28b3779dd20befd127c14f1bbf9a3916
Italo Silva <italo@maleldil.com>
2016-10-11 18:27:20 -0300

    Ajustes nas funções de experimento do AG

--- src/faptp-app/main.cpp
+++ src/faptp-app/main.cpp
@@ -81,137 +81,46 @@ void semArgumentos()
     out << r.getLog();
 }
 
-void teste()
-{
-    Resolucao r {Configuracao()
-            .arquivoEntrada(Util::join_path({"entradas"}, "input.all.json"))
-            .populacaoInicial(1)
-            .numIteracoes(0)
-            .porcentagemSolucoesAleatorias(0) // %
-            .porcentagemCruzamentos(0) // %
-            .numMaximoIteracoesSemEvolucaoGRASP(25)
-            .tipoCruzamento(Configuracao::TipoCruzamento::ciclo)
-            .tipoMutacao(Configuracao::TipoMutacao::substiui_disciplina)
-            .mutacaoProbabilidade(30) // %
-            .graspTempoConstrucao(600) // ms
-            .graspNumVizinhos(2)
-            .graspAlfa(30) // %
-            .camadaTamanho(40)
-            .perfilTamanho(1400)
-            .numTorneioPares(0)
-            .numTorneioPopulacao(1)
-            .tentativasMutacao(2)
-            .graspVizinhanca(Configuracao::TipoVizinhos::aleatorios)
-            .tipoConstrucao(Configuracao::TipoGrade::modelo)
-            //.tipoConstrucao(Configuracao::TipoGrade::grasp)
-            };
-
-    r.teste();
-}
-
-std::pair<long long, double>
+std::pair<long long, int>
 novo_experimento(const std::string& input, const std::string& id,
                  int n_indiv, int taxa_mut, int p_cruz,
-                 const std::string& oper_cruz,
-                 int grasp_iter, int ag_iter,
+                 const std::string& oper_cruz, int grasp_iter, 
                  int grasp_nviz, int grasp_alfa, int n_tour, int n_mut,
                  int exec_i)
 {
-    auto timenow = Output::timestamp();
-    auto execstr = "Exec" + std::to_string(exec_i);
+    // número de iterações grande para o algoritmo se encerrar por tempo
+    const auto inf = gsl::narrow_cast<int>(1e9); 
 
-    Configuracao::TipoCruzamento cruz {};
-    if (oper_cruz == "PMX")
-        cruz = Configuracao::TipoCruzamento::pmx;
-    else if (oper_cruz == "CX")
-        cruz = Configuracao::TipoCruzamento::ciclo;
-    else if (oper_cruz == "OX")
-        cruz = Configuracao::TipoCruzamento::ordem;
-    else
-        cruz = Configuracao::TipoCruzamento::pmx;
+    auto cruzamento = [&] {
+        if (oper_cruz == "PMX") {
+            return Configuracao::TipoCruzamento::pmx;
+        } else if (oper_cruz == "CX") {
+            return Configuracao::TipoCruzamento::ciclo;
+        } else if (oper_cruz == "OX") {
+            return Configuracao::TipoCruzamento::ordem;
+        }
+    }();
 
     Resolucao r {Configuracao()
             .arquivoEntrada(input)
             .populacaoInicial(n_indiv)
             .porcentagemCruzamentos(p_cruz) // %
             .numMaximoIteracoesSemEvolucaoGRASP(grasp_iter)
-            .numMaximoIteracoesSemEvolucaoAG(ag_iter)
-            .tipoCruzamento(cruz)
-            .tipoMutacao(Configuracao::TipoMutacao::substiui_disciplina)
+            .numMaximoIteracoesSemEvolucaoAG(inf)
+            .tipoCruzamento(cruzamento)
             .mutacaoProbabilidade(taxa_mut) // %
             .graspNumVizinhos(grasp_nviz)
             .graspAlfa(grasp_alfa) // %
-            .camadaTamanho(33)
-            .perfilTamanho(1400)
-            .numTorneioPares(0)
+            .camadaTamanho(input_all_json.camadasTamanho)
+            .perfilTamanho(input_all_json.perfilTamanho)
             .numTorneioPopulacao(n_tour)
             .tentativasMutacao(n_mut)
-            .graspVizinhanca(Configuracao::TipoVizinhos::aleatorios)
-            .tipoConstrucao(Configuracao::TipoGrade::grasp)
-            };
+    };
 
-    auto inicio = Util::now();
+    Timer t;
     r.gerarHorarioAG();
-    auto fim = Util::now();
-    auto tempo = Util::chronoDiff(fim, inicio);
     auto fo = r.getSolucao()->getFO();
-
-    return {tempo, fo};
-}
-
-std::pair<long long, double>
-novo_experimento_fase2(const std::string& input, const std::string& id, int exec_i,
-                       int ag_iter, Configuracao::TipoCruzamento tipo_cruz)
-{
-    auto timenow = Output::timestamp();
-    auto execstr = "Exec" + std::to_string(exec_i);
-    //auto path = Util::join_path({"exper", "log", id + execstr});
-    //Util::create_folder(path);
-    //auto fileout = path + "result" + id + execstr + ".txt";
-    //std::cout << id << "\n";
-    //std::cout << "File: " << fileout << "\n";
-
-    Resolucao r {Configuracao()
-            .arquivoEntrada(input)
-            .populacaoInicial(40)
-            .porcentagemCruzamentos(30) // %
-            .numMaximoIteracoesSemEvolucaoGRASP(25)
-            .numMaximoIteracoesSemEvolucaoAG(ag_iter)
-            .tipoCruzamento(tipo_cruz)
-            .tipoMutacao(Configuracao::TipoMutacao::substiui_disciplina)
-            .mutacaoProbabilidade(30) // %
-            .graspNumVizinhos(2)
-            .graspAlfa(60) // %
-            .camadaTamanho(40)
-            .perfilTamanho(1400)
-            .numTorneioPares(0)
-            .numTorneioPopulacao(4)
-            .tentativasMutacao(2)
-            .graspVizinhanca(Configuracao::TipoVizinhos::aleatorios)
-            .tipoConstrucao(Configuracao::TipoGrade::grasp)
-            };
-
-    auto inicio = Util::now();
-    r.gerarHorarioAG();
-    auto fim = Util::now();
-    auto tempo = Util::chronoDiff(fim, inicio);
-
-    //auto saida = std::ofstream(fileout);
-    //saida << id << "\n";
-    //saida << execstr << "\n";
-    //saida << "Tempo do horario: " << tempo << "ms\n";
-
-    auto fo = r.getSolucao()->getFO();
-    //saida << "\nResultado:" << fo << std::endl;
-
-    //saida << "hash: " << r.hashAlvo << "\n";
-    //saida << "solucaoAlvo:" << r.foAlvo << "\n";
-    //saida << "iteracoes:" << r.iteracaoAlvo << "\n";
-    //saida << "tempoAlvo:" << r.tempoAlvo << "\n";
-    //saida << "ultima iteracao: " << r.ultimaIteracao << "\n\n";
-    //saida << r.getLog() << "\n";
-
-    //Output::write(r.getSolucao(), path);
+    auto tempo = t.elapsed();
 
     return {tempo, fo};
 }
@@ -222,15 +131,15 @@ void novo_experimento_cli(const std::string& input, const std::string& file)
 
     // comentário do topo
     // formato entrada:
-    // ID AGIter TaxaMut NIndiv %Cruz CruzOper NMut NTour GRASPIter GRASPNVzi GRASPAlfa NExec
+    // ID TaxaMut NIndiv %Cruz CruzOper NMut NTour GRASPIter GRASPNVzi GRASPAlfa NExec
     // formato saida:
     // ID,NExec,Tempo,Fo
 
     std::string id, cruz_oper;
-    int ag_iter, taxa_mut, n_indiv, p_cruz, n_mut, n_tour, grasp_iter;
+    int taxa_mut, n_indiv, p_cruz, n_mut, n_tour, grasp_iter;
     int grasp_nviz, grasp_alfa, n_exec;
 
-    while (config >> id >> ag_iter >> taxa_mut >> n_indiv >> p_cruz
+    while (config >> id >> taxa_mut >> n_indiv >> p_cruz
         >> cruz_oper >> n_mut >> n_tour
         >> grasp_iter >> grasp_nviz >> grasp_alfa >> n_exec) {
 
@@ -238,76 +147,19 @@ void novo_experimento_cli(const std::string& input, const std::string& file)
         Util::create_folder(path);
 
         auto filename = path + id + ".txt";
-        std::ofstream out {filename};
 
         std::cout << "\n\nID: " << id << "\n";
 
-        out << "ID Algoritmo, Numero execucao, Tempo total, FO\n";
         for (auto i = 0; i < n_exec; i++) {
             long long tempo;
-            double fo;
+            int fo;
             std::tie(tempo, fo) = novo_experimento(
                 input, id, n_indiv, taxa_mut, p_cruz, cruz_oper, grasp_iter,
-                ag_iter, grasp_nviz, grasp_alfa, n_tour, n_mut, i);
+                grasp_nviz, grasp_alfa, n_tour, n_mut, i);
 
-            std::cout << id << "," << i << "," << tempo << "," << fo << "\n";
-            out << id << "," << i << "," << tempo << "," << fo << "\n";
-        }
-    }
-}
-
-void novo_experimento_cli_fase2(const std::string& input, const std::string& file)
-{
-    std::ifstream config {file};
-
-    // comentário do topo
-    // formato entrada:
-    // ID AGIter NIndiv %Cruz NMut NTour GRASPIter GRASPNVzi GRASPAlfa NExec
-    // formato saida:
-    // ID,NExec,Tempo,Fo
-
-    std::string id {};
-    int ag_iter {};
-    int xover_id {};
-    int n_exec {};
-    Configuracao::TipoCruzamento xover {};
-
-    while (config >> id >> ag_iter >> xover_id >> n_exec) {
-        switch (xover_id) {
-            case 1:
-                xover = Configuracao::TipoCruzamento::pmx;
-                break;
-            case 2:
-                xover = Configuracao::TipoCruzamento::ordem;
-                break;
-            case 3:
-                xover = Configuracao::TipoCruzamento::ciclo;
-                break;
-            default:
-                xover = Configuracao::TipoCruzamento::pmx;
-                break;
-        }
-
-        auto path = Util::join_path({"exper", "result"});
-        Util::create_folder(path);
-
-        auto filename = path + "result" + id + ".txt";
-        std::ofstream out {filename};
-
-        std::cout << "\n\nID: " << id << "\n";
-
-        out << "ID Algoritmo, Numero execucao, Tempo total, FO\n";
-        for (auto i = 0; i < n_exec; i++) {
-            long long tempo;
-            double fo;
-            std::tie(tempo, fo) = novo_experimento_fase2(
-                input, id, i, ag_iter, xover);
-
-            std::cout << id << "," << i << "," << tempo << "," << fo << "\n";
-            out << id << "," << i << "," << tempo << "," << fo << "\n";
-
-            // TODO:
-            // Refatorar os métodos de crossover (DRY)
+            std::ofstream out {filename};
+            out << "ID Algoritmo, Numero execucao, Tempo total, FO\n";
+            Util::logprint(out, boost::format("%s,%d,%lld,%d\n") % id % i % tempo % fo);
         }
     }
 }
@@ -315,9 +167,6 @@ void novo_experimento_cli_fase2(const std::string& input, const std::string& fil
 template <typename F>
 std::string teste_tempo_iter(int num_exec, F f)
 {
-    using Util::logprint;
-    using boost::format;
-
     std::ostringstream oss;
 
     for (auto i = 0; i < num_exec; i++) {
@@ -328,16 +177,17 @@ std::string teste_tempo_iter(int num_exec, F f)
             .tentativasMutacao(4)
         };
 
-        logprint(oss, format("i: %d") % (i+1));
+        Util::logprint(oss, boost::format("i: %d") % (i+1));
 
         Timer t;
         auto s = f(r);
 
-        logprint(oss, format(" - fo: %d - t: %lld\n") % s->getFO() % t.elapsed());
-        logprint(oss, format("\t fo_alvo: %d - tempo_alvo: %lld\n") % r.foAlvo % r.tempoAlvo);
+        Util::logprint(oss, boost::format(" - fo: %d - t: %lld\n") % s->getFO() % t.elapsed());
+        Util::logprint(oss, 
+            boost::format("\t fo_alvo: %d - tempo_alvo: %lld\n") % r.foAlvo % r.tempoAlvo);
     }
 
-    logprint(oss, "\n");
+    Util::logprint(oss, "\n");
 
     return oss.str();
 }
@@ -375,75 +225,6 @@ void teste_tempo()
     out << oss.str() << std::endl;
 }
 
-void teste_sa_ils()
-{
-    Timer t;
-
-    Resolucao r{Configuracao()
-        .arquivoEntrada(Util::join_path({"entradas"}, "input.all.json"))
-        .camadaTamanho(input_all_json.camadasTamanho)
-        .perfilTamanho(input_all_json.perfilTamanho)
-        .tentativasMutacao(4)
-    };
-
-    auto s = r.gerarHorarioSA_ILS(30'000);
-
-    std::cout << "\nResultado:" << s->getFO() << "\n";
-    std::cout << "Tempo: " << t.elapsed() << "\n";
-
-    /*
-    auto savePath = Util::join_path({"teste", "fo" + std::to_string(fo)});
-    Output::write(s.get(), savePath);
-    */
-}
-
-void teste_wdju()
-{
-    auto antes = Util::now();
-
-    Resolucao r{Configuracao()
-        .arquivoEntrada(Util::join_path({"entradas"}, "input.all.json"))
-        .camadaTamanho(input_all_json.camadasTamanho)
-        .perfilTamanho(input_all_json.perfilTamanho)
-        .tentativasMutacao(4)
-    };
-
-    auto s = r.gerarHorarioWDJU(30'000);
-
-    auto fo = s->getFO();
-    auto tempo = Util::chronoDiff(Util::now(), antes);
-
-    std::cout << "\nResultado:" << fo << "\n";
-    std::cout << "Tempo: " << tempo << "\n";
-
-    auto savePath = Util::join_path({"teste", "fo" + std::to_string(fo)});
-    Output::write(s.get(), savePath);
-}
-
-void teste_hysst()
-{
-    std::cout << "HySST\n";
-
-    Timer t;
-    Resolucao r{Configuracao()
-        .arquivoEntrada(Util::join_path({"entradas"}, "input.all.json"))
-        .camadaTamanho(input_all_json.camadasTamanho)
-        .perfilTamanho(input_all_json.perfilTamanho)
-        .tentativasMutacao(4)
-    };
-
-    auto s = r.gerarHorarioHySST(30'000, 100, 100);
-
-    auto fo = s->getFO();
-    auto tempo = t.elapsed();
-
-    std::cout << "Resultado:" << fo << "\n";
-    std::cout << "Tempo: " << tempo << "\n";
-
-    auto savePath = Util::join_path({"teste", "fo" + std::to_string(fo)});
-    Output::write(s.get(), savePath);
-}
-
 int main(int argc, char* argv[])
 {
     if (argc == 3) {
@@ -459,9 +240,6 @@ int main(int argc, char* argv[])
         }
     } else {
         //semArgumentos();
-        //teste_sa_ils();
-        //teste_wdju();
-        //teste_hysst();
 
         teste_tempo();
     }
